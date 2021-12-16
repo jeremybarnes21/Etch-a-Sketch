9e0d2525e609d599ad4351f8f41ec2684fe08cb0
Jeremy Barnes <jeremypb21@gmail.com>
2021-12-16 15:30:50 -0500

    Working app... need to see if can get buttons to change color of drawing

--- app.js
+++ app.js
@@ -9,28 +9,56 @@ function random_bg_color() {
     let y = Math.floor(Math.random() * 256);
     let z = Math.floor(Math.random() * 256);
     bColor = "rgb(" + x + "," + y + "," + z + ")";
-    console.log(bColor);
+    //console.log(bColor);
 }
 random_bg_color();
 
 
 function gridLayout(rowColumns){
-    container.style.gridTemplateColumns = `repeat(${rowColumns}, 1fr)`; //Learned about string interpolation... this is NOT jquery
+    container.style.gridTemplateColumns = `repeat(${rowColumns}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${rowColumns}, 1fr)`;
     for(i=0; i<rowColumns*rowColumns;i++){
         const square = document.createElement('div');
         square.classList.add('grid-item');
         container.appendChild(square);
-        square.addEventListener('mouseenter', ()=>{
-            console.log('mouse has entered');
+        square.addEventListener('mouseenter', ()=>{ //need to have this function take a variable that changes with each thing... need to look into syntax more.
+           // console.log('rainbow');
             random_bg_color();
+           // console.log(bColor);
             square.style.backgroundColor = bColor; 
         });
+        //Need to make branch at home some do not mess up old code.
+        //This may be helpful: how to pass argument event listener stack overflow
+        /*
+        const blackBtn = document.querySelector('#black');
+        blackBtn.addEventListener('click',()=>{
+
+        });
+
+        const blueBtn = document.querySelector('#blue');
+        blueBtn.addEventListener('click',()=>{
+    
+        });
+
+        const greenBtn = document.querySelector('#green');
+        greenBtn.addEventListener('click',()=>{
+    
+        });
+
+        const rainbowBtn = document.querySelector('#rainbow');
+        rainbowBtn.addEventListener('click',()=>{
+            square.addEventListener('mouseenter', ()=>{
+                console.log('rainbow');
+                random_bg_color();
+                square.style.backgroundColor = bColor; 
+            });*/ //This needs to be added first I think...
+        }
+        
     }
-}
+
 gridLayout(rowColumns);
 
-const resetButton = document.querySelector('#btn');
+const resetButton = document.querySelector('#reset');
 
 resetButton.addEventListener('click',()=>{
     rowColumns = prompt('Enter your grid dimensions below. For example, if you want your grid to be 20x20, enter "20". Do not use a grid more detailed than 100x100.');
@@ -41,7 +69,10 @@ resetButton.addEventListener('click',()=>{
     for(div of divs){
         div.style.backgroundColor = 'white';
      }
+    const btnContainer = document.querySelector('#btncontainer');
+    btnContainer.style.backgroundColor = '#828e84';
     gridLayout(rowColumns);
     
 });
-//12.14.21 app is now functioning!!! Now can add styling and potentially random colors 
+
+
